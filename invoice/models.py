fa73f3dfa219467f0bb11d845cf51cfa7a277623
Your Name <you@example.com>
2021-02-27 13:17:21 +0530

    Edit Gitignore

--- invoice/models.py
+++ invoice/models.py
@@ -109,7 +109,7 @@ class Compony(models.Model):
     company_phone = models.CharField(max_length=150)
     company_logo = models.TextField()
     company_signature = models.TextField()
-    company_admin = models.CharField(max_length=150)
+    
     company_id = models.CharField(max_length=150,default=str(uuid.uuid4())[:8],primary_key=True)
 
 
