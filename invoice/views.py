6766523fab65ce8d1249e4bc2b39189505a162dc
Your Name <you@example.com>
2021-03-05 14:42:29 +0530

    Excel Modeule Added

--- invoice/views.py
+++ invoice/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, permissions,viewsets
 from rest_framework.response import Response
 from .serializers import *
 import  os
+from django.core.files.storage import FileSystemStorage
 import pandas
 from django.db.models import Case, CharField, Value, When
 from django.core.paginator import Paginator
@@ -503,13 +504,20 @@ class SortForCompany(generics.ListAPIView):
 class ExcelConvert(generics.CreateAPIView):
     serializer_class = ExcelConvert
     def post(self,request):
-        print(request.data)
-        # excelfile = request.FILES.get("excelfile")
-        # xls = ExcelFile('static/tests-example.xls')
-        travel_df = pandas.read_excel('static/inventory.xlsx')
-        cities = travel_df.to_dict('records')
-        print(cities[0])
-        return Response({"data":cities})
+        request_file = request.FILES['files'] if 'files' in request.FILES else None
+        if request_file: 
+             
+            fs = FileSystemStorage() 
+            file = fs.save(request_file.name, request_file) 
+            
+            fileurl = fs.url(file) 
+            print(fileurl)
+        conver_to_pdf = pandas.read_excel('static/img/inventory.xlsx')
+        excel_datas = conver_to_pdf.to_dict('records')
+    
+        for i in range(len(excel_datas)):
+            inventory = Inventory.objects.create(name=excel_datas[i]['name'],hsn=excel_datas[i]['hsn'],base_price=excel_datas[i]['base_price'],sales_price=excel_datas[i]['sales_price'],stock=excel_datas[i]['stock'],unit=excel_datas[i]['unit'],cgst=excel_datas[i]['cgst'],sgst=excel_datas[i]['sgst'],cess=excel_datas[i]['cess'],others=excel_datas[i]['others'],barcode=excel_datas[i]['barcode'])
+        return Response({"data":excel_datas})
 
     
 
